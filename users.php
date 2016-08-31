5aef8013b8259e9a25f19c1d55e64cc3265bdc76
Josh Hall <joshhall9@hotmail.com>
2016-08-31 01:13:48 -0700

    adds some validation

--- users.php
+++ users.php
@@ -23,22 +23,27 @@
 		}
 	}
 	else if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		$email = $_POST['email'];
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
-		$gender = $_POST['gender'];
-		$dob = $_POST['dob'];
-		$query = "SELECT addNewPerson('$email','$first_name','$last_name', '$gender', '$dob')";
-		$result = mysqli_query($db, $query);
-		if($result){
-			while($r = mysqli_fetch_assoc($result)) {
-	   			$rows['result'][] = $r;
-	   		}
-	   		if($rows['result'][0]["addNewPerson('$email','$first_name','$last_name', '$gender', '$dob')"] == 1){
-	   			deliver_response(200, "user successfully added", NULL);
+		if(!empty($_POST['email']) && !empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['gender']) && !empty($_POST['dob'])){
+			$email = $_POST['email'];
+			$first_name = $_POST['first_name'];
+			$last_name = $_POST['last_name'];
+			$gender = $_POST['gender'];
+			$dob = $_POST['dob'];
+			$query = "SELECT addNewPerson('$email','$first_name','$last_name', '$gender', '$dob')";
+			$result = mysqli_query($db, $query);
+			if($result){
+				while($r = mysqli_fetch_assoc($result)) {
+		   			$rows['result'][] = $r;
+		   		}
+		   		if($rows['result'][0]["addNewPerson('$email','$first_name','$last_name', '$gender', '$dob')"] == 1){
+		   			deliver_response(200, "user successfully added", NULL);
+				}
+				else{
+					deliver_response(400, "user already exists", NULL);
+				}
 			}
 			else{
-				deliver_response(400, "user already exists", NULL);
+				deliver_response(400, "invalid request", NULL);
 			}
 		}
 		else{
